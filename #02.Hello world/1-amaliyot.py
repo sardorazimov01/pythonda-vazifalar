85a7c7fa334c9241ea46b4a23507dbe7beb37608
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-01-17 15:27:43 +0500

    2-dars vazivalarim .-. Qushildi
    Co-Authored-By: Nurbek Po'latov <88614410+nurbekdev@users.noreply.github.com>

---
+++ #02.Hello world/1-amaliyot.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 15:22:26 2022
+
+@author: Sardor 
+
+       02 Hellow World!  
+         
+
+AMALIYOT
+Spyder konsolida va matn muharririda quyidagi kodlarni yozib, bajarib ko'ring:
+    
+"""
+# -----------
+print('Assalom alaykum')
+# -----------
+print("Hayrli tong!")
+# -----------
+print(2+4*2)
+# -----------
+print(19/5)
+# -----------
+print(2**4)
+
+   
