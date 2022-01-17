93e2176fa3d5da85a1161a03df770e702aaeaa9f
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-01-17 15:58:50 +0500

    vazifalar bajarildi
    Co-Authored-By: Nurbek Po'latov <88614410+nurbekdev@users.noreply.github.com>

---
+++ #04.O'zgaruvchilar/1-amaliyot.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 15:51:11 2022
+
+@author:Sardor
+
+      04 O'zgaruvchilar
+
+"""
+
+
+# AMALIYOT
+
+# Quyidagi mashqlarni bajaring:
+
+# 1. "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring.
+# 2. xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+
+# 3.Quyidagi kodni bajaring:
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+                             # Javoblar
+# 1.
+# a="Hello World"
+# print(a)
+
+
+# 2.
+# xabar="bugun dam olish kuni"
+# print(xabar)
+
+
+# 3.
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+
+# 4.
+
