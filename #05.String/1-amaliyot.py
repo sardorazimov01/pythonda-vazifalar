cfc59d58e00d9ed4c1fa11518e49326093f276ee
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-01-17 16:20:22 +0500

    vazifalar bajarildi
    Co-Authored-By: Nurbek Po'latov <88614410+nurbekdev@users.noreply.github.com>

---
+++ #05.String/1-amaliyot.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 15:58:31 2022
+
+@author:   Sardor
+
+      05 String(Matn)
+      
+"""
+
+
+# 1. Quyidagi o'zgaruvchilarni yarating: 
+    
+    
+# kocha="Bog'bon"
+# mahalla="Sog'bon"
+# tuman="Bodomzor" 
+# viloyat="Samarqand"
+# print(f"{kocha} ko'chasi {mahalla} mahallasi {tuman} tumani {viloyat} viloyati" )
+
+
+# 3. Yuqoridagi o'zgaruvchilarning (kocha, mahalla, tuman, viloyat) qiymatini 
+# foydalanuvchidan so'rang. Va avvalgi mashqni takrorlang.
+
+# kocha=str(input("ko'changizni kiriting: "))
+# mahalla=str(input("mahallangizni kiriting: "))
+# tuman=str(input("tumaningizni kiriting: "))
+# viloyat=str(input("viloyatingizni kiriting: "))
+# print(f"{kocha} ko'chasi {mahalla} mahallasi {tuman} tumani {viloyat} viloyati" )
+
+
+
+# 4. Yuqoridagi matnni konsolga chiqarishda har bir verguldan keyin yangi qatordan yozing
+
+
+# kocha=str(input("ko'changizni kiriting: "))
+# mahalla=str(input("mahallangizni kiriting: "))
+# tuman=str(input("tumaningizni kiriting: "))
+# viloyat=str(input("viloyatingizni kiriting: "))
+# print( f"{kocha} ko'chasi \n{mahalla} mahallasi \n{tuman} tumani \n{viloyat} viloyati" )
+
+
+# 5. Yuqoridagi matnni f-string yordamida, yangi, manzil deb nomlangan o'zgaruvchiga yuklang
+
+
+# kocha=str(input("ko'changizni kiriting: "))
+# mahalla=str(input("mahallangizni kiriting: "))
+# tuman=str(input("tumaningizni kiriting: "))
+# viloyat=str(input("viloyatingizni kiriting: "))
+# print( f"Yangi manzil \n{kocha} ko'chasi \n{mahalla} mahallasi \n{tuman} tumani \n{viloyat} viloyati" )
+
+
+# 6. manzilga biz yuqorida o'rgangan title(), upper(), lower() , capitalize() metodlarini qo'llab ko'ring.
+
+# kocha=str(input("ko'changizni kiriting: "))
+# mahalla=str(input("mahallangizni kiriting: "))
+# tuman=str(input("tumaningizni kiriting: "))
+# viloyat=str(input("viloyatingizni kiriting: "))
+# print( f"Yangi manzil \n{kocha.title()} ko'chasi \n{mahalla.lower()} mahallasi \n{tuman.upper()} tumani \n{viloyat} viloyati" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
