487eac6529727fa753b939a5830e651f24e19ff6
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-01-17 15:20:55 +0500

    1-dars javoblari qo'shildi
    Co-Authored-By: Nurbek Po'latov <88614410+nurbekdev@users.noreply.github.com>

---
+++ #01.Kerakli dasturlar/kerakli_dasturlar.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 15:09:19 2022
+
+@author: User
+
+Biz uchun kerakli dasturlar.
+----
+Python3
+----
+Syder - Python tilida kod yozish uchun mo'ljallangan dasturlash
+muhiti va bizning asasiy qurolimizdir.Ingliz tilida dasturlash 
+muhiti - IDE (Integrated Development Environment) deyiladi..
+
+"""
+
+print("Spyder")
