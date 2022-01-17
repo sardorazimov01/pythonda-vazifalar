af70535c2dd99b78689a35bfc584bca03086eb21
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-01-17 15:50:51 +0500

    vazifalar bajarildi
    Co-Authored-By: Nurbek Po'latov <88614410+nurbekdev@users.noreply.github.com>

---
+++ #03.Print()-sintaks va arifmtik amallar/1-amaliyot.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 15:29:52 2022
+
+@author: Sardor
+
+        03 Print(), sinteks va arifmetik amallar.
+
+"""
+
+# AMALIYOT
+
+# Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
+#        "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+
+# Quyidagi misollarga yechimni Pythonda chiqaring. Har bir misoldan avval misol matnini izoh ko'rinishida yozing:
+    
+# 1 . 5 ning 4-darajasini toping
+
+# 2. 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+
+# 3. Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+
+# 4. Diametri 12 ga teng bo'lgan doiraning yuzini toping  (pi = 3.14 deb oling)
+
+# 5. Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+
+
+# ------------
+print("Nexia", "Tico",'Damas', "Korganlar qilar qavas")
+
+
+# 1.
+print('Beshning tortinchi darajasi', 5**4,'ga teng')
+
+# 2.
+print(22//4, " qoldiq qoladi")
+
+
+3.
+tomon=125 
+peremetri= (tomon+tomon)*2
+print(peremetri)
+
+
+# 4.
+diametr = 12
+pi = 3.14
+yuza = diametr*pi
+print(yuza)
+
+
+# 5.
+katet1=6
+katet2=7
+gipotenuza = katet1**2+katet2**2
+print(gipotenuza)
+
+
+#
+
+
+
+
+
