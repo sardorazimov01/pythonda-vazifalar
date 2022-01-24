baa7947074e83973855dab6b1a6e2d874ce9322d
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-01-24 15:59:46 +0500

    Lugat.py
    vazifalar bajarildi

---
+++ #14.Lugatlar/amalyotlar yig'indisi.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 14:54:16 2022
+
+@author:  Sardor
+
+    Lug'at
+    
+"""
+        # 1-amaliyot
+# 1. otam (onam, akam, ukam, va hokazo) degan lug'at yarating va lug'atga 
+# shu inson haqida kamida 3 ta m'alumot kiriting (ismi, tu'gilgan yili, shahri, manzili va hokazo).
+ # Lug'atdagi ma'lumotni matn shaklida konsolga chiqaring :Otamning ismi Mavlutdin, 1954-yilda, Samarqand viloyatida tug'ilgan
+# tug'ilgan.
+
+# ism= {'ism':'sardor azimov', 't_yil':2007 , 'shahar':'shahrisabz'}
+# print(f"Mening ismim {ism['ism'].title()} , {ism['t_yil']}- yilda ,{ism ['shahar'].title()} shahrida tug'ilganman")
+
+# print("   ")
+# print('1-amaliyot yakunlandi')
+
+                # 2-amaliyot
+# 2 . Oila a'zolaringizning sevimli taomlari lug'atini tuzing. Lug'atda kamida 5 ta ism-taom jufltigi bo'lsin. 
+# Kamida uch kishining sevimli taomini konsolga chiqaring: Alining sevimli taomi osh
+
+
+
+# taomlar = {
+#     'sardor':'shashlik',
+#     'umar':'osh',
+#     'asil':"lavaash",
+#     'doston':"hotdog",
+#     'olim':"somsa"
+#     }
+
+# taom = taomlar['sardor']
+# print(f"Sardorning sevimli taomi {taom}")
+# print("   ")
+# print('Dastur tugati')
+                        # 3-amaliyot
+
+# python_izohli_lugati = {
+#     'integer':"Butun son",
+#     'float':"O'nlik son",
+#     'string':"Matn",
+#     'list':"Ro'yxat",
+#     'tuple':"O'zgarmas ro'yxat"}
+
+
+# lugat= input("Kalit so'z kiriting:").lower()
+# print(python_izohli_lugati.get(lugat,"Bunday so'z mavjud emas"))
+
+
+
+
+                  # 4-amaliyot
+# 4.Foydalanuvchidan biror so'z kiritishni so'rang va so'zning tarjimasini yuqoridagi lug'atdan chiqarib bering.  
+ # Agar so'z lu'gatda mavjud bo'lmasa, "Bunda so'z mavjud emas" degan xabarni chiqaring.
+
+# python_izohli_lugati = {
+#     'integer':"Butun son",
+#     'float':"O'nlik son",
+#     'string':"Matn",
+#     'list':"Ro'yxat",
+#     'tuple':"O'zgarmas ro'yxat"}
+
+
+
+# lugat = input("Kalit so'z kiriting:").lower()
+# tarjima = python_izohli_lugati.get(lugat)
+# if tarjima==None:
+#     print("Bunday so'z mavjud emas")
+# else:
+#     print(f"{lugat.title()} so'zi {tarjima} deb tarjima qilinadi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 5.Yuqoridagi vazifani if-else yordamida qiling va natijani ham foydalanuvchiga tushunarli ko'rinishda chiqaring. 
