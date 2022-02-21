be01d3403b6db2f43f2c70ef2849637559510ad7
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-02-21 16:31:41 +0500

    vazifalar bajarildi
    vazifalar bajarildi

---
+++ #19.funktions/1-amaliyot.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 16:12:47 2022
+
+@author: User
+"""
+
+# def aftobiagrafiya(ism,familya,t_yil):
+#     """ism,familya,t_yil va yoshni topadigan fuksiya"""
+#     print(f"{ism.title()} {familya.title()} {t_yil}-da tug'ilgan hozir {2021-t_yil} yoshda..")
+    
+    
+    
+# aftobiagrafiya('sardor', 'azimov', t_yil=2007)
+
+
+
