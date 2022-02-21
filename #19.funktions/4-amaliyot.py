60aac5b014fd6242d2270cf6f0a367f4ff43f9d7
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-02-21 16:50:59 +0500

    vazifalar bajarildi
    vazifalar bajarildi

---
+++ #19.funktions/4-amaliyot.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 16:42:49 2022
+
+
+@author: functions
+
+Azimov_Sardorbek
+
+
+"""
+
+
+def kattasi_top(son1,son2):
+    """2-ta sonning katta yoki tengligini topadigan funksiya"""
+    if son1<son2:
+        print(f"{son2} katta")
+    elif son1>son2:
+        print(f"{son1} katta")    
+    else:
+        print("teng")
+        
+        
+kattasi_top(77, 77)
+        
