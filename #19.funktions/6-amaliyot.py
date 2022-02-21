df9547badb5579105c415a2be0956a5f4083ea60
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-02-21 17:03:09 +0500

    vazifalar bajarildi
    vazifalar bajarildi

---
+++ #19.funktions/6-amaliyot.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 16:58:57 2022
+
+@author: functions
+
+     Azimov_Sardorbek
+
+"""
+
+def kvadrat_top(son,daraja=2):
+    """sonning kavadratini hisoblaydigan funksiya"""
+    print(f"{son} sonining kvadrati {son**daraja} ga teng")
+    
+    
+kvadrat_top(2)
