402e35ca61e2914ab37df2e6cf41f622a297a77d
Sardor..azIm0v <sardorazimov843@gmail.com>
2022-02-21 16:37:21 +0500

    vazifalar bajarildi
    vazifalar bajarildi

---
+++ #19.funktions/2-amaliyot.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 16:32:04 2022
+
+@author: User
+"""
+
+def kvadrat_kub_top(son):
+    """sonning kvadrati va kubini topadigan funksiya"""
+    print(f"Siz kiritgan sonning kvadrati-{son**2}, kubi-{son**3}-ga teng")
+    
+    
+kvadrat_kub_top(777)
